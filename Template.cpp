e198968a92e9d126a6f49410edfe179a7807be4e
Harshaljain <66555099+Harshal0506@users.noreply.github.com>
2021-05-02 18:09:30 +0530

    Added Comment

--- Template.cpp
+++ Template.cpp
@@ -20,7 +20,7 @@ using namespace std;
 #define ps(x,y)         fixed<<setprecision(y)<<x
 #define mk(arr,n,type)  type *arr=new type[n];
 #define w(x)            int x; cin>>x; while(x--)
-mt19937                 rng(chrono::steady_clock::now().time_since_epoch().count());
+mt19937                 rng(chrono::steady_clock::now().time_since_epoch().count()); // for random number generation / suffling
  
 typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> pbds;
  
